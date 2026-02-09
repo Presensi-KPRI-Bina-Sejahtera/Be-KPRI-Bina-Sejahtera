4474636b8a6f87b112b8847a12529d746427f503
Fratsia <fauzantrisuladana@gmail.com>
2026-02-09 22:50:28 +0700

    feat: Update user relationship in Attendance model to include soft-deleted users

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -26,6 +26,6 @@ class Attendance extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 }
